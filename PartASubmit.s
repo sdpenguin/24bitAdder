44ed44923c7f053894dde658943d73c30eb9ef74
Waleed El-Geresy <noEmail>
2016-12-30 21:46:19 +0000

    Shortened by adding more efficiently

--- PartASubmit.s
+++ PartASubmit.s
@@ -5,18 +5,13 @@ MUL24X24
 		MOV		R6,#32 ;The amount of shift for R3
 		MOV		R2,#0x0 ;Initialise the sum to 0
 		MOV		R3,#0x0
-		MOV		R8,#1
 LOOP		ANDS		R7,R1,R4 ;R5 holds the ANDed values
-		ADDSNE	R2,R2,R0, LSL R5 ;If the result is non-zero then there exists a bit at that position
-		ADDCS	R3,R3,#1 ;If there's an unsigned overflow
-		ANDS		R7,R1,R4
 		ADDNE	R3,R3,R0, LSR R6
+		ADDSNE	R2,R2,R0, LSL R5 ;If the result is non-zero then there exists a bit at that position
+		ADDCS	R3,R3,#1 ;If there's an unsigned overflow - note the s here refers to the carry set as opposed to changing condition bits which would be ADDSCS
 		LSL		R4,R4,#1
 		ADD		R5,R5,#1
 		SUB		R6,R6,#1
-
-		LSLS		R0,R0,
-		
 		CMP		R4,#0x1000000 ;Compare R4 to 2^24 (this means that this is the 24th bit shift and this signals the end
 		BNE		LOOP
 		LDMED	R13!,{R4-R12,LR}
