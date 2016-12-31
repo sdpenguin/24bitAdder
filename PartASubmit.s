701ec567d9d690b901c83122ca97b28b6656512a
Waleed El-Geresy <noEmail>
2016-12-31 21:08:35 +0000

    Much faster, working code. Worst case is 157 with 15 instructions and 296 cycles, but the best case is under 50 cycles!

--- PartASubmit.s
+++ PartASubmit.s
@@ -1,19 +1,15 @@
-		MOV		R0,#1
-		MOV		R1,#0x000000FF
-		BL		MUL24X24
-		END
-		
 MUL24X24	STMED	R13!,{R0,R1,R4-R12,LR}
-		MOV		R8,#0x0 ;Counter
+		MOV		R4,#0 ;continuation of R0
 		MOV		R2,#0x0 ;Initialise the sum to 0
 		MOV		R3,#0x0
-LOOP		ANDS		R7,R1,#1 ;R7 is -1 if first bit of R1 is 1
-		BEQ		SKIP
-		RSB		R6,R8,#32
-		ADDS		R2,R2,R0, LSL R8
-		ADC		R3,R3,R0, LSR R6 ;Add with a carry if carry set
-SKIP		ADD		R8,R8,#1
 		LSRS		R1,R1,#1
+		BCC		LOOP
+ADDER	RSB		R5,R4,#32
+		ADDS		R2,R2,R0,LSL R4
+		ADC		R3,R3,R0,LSR R5
+LOOP		LSRS		R1,R1,#1
+		ADD		R4,R4,#1
+		BCS		ADDER
 		BNE		LOOP
 		LDMED	R13!,{R0,R1,R4-R12,LR}
 		MOV		PC,LR
--- projtb.s
+++ projtb.s
@@ -57,16 +57,17 @@ PROJTBDATA15	DCD		0x103D62, 0x4380C7, 0x3BF6B72E, 0x448
 			
 			
 MUL24X24		STMED	R13!,{R0,R1,R4-R12,LR}
-			MOV		R8,#0x0 ;Counter
+			MOV		R4,#0 ;continuation of R0
 			MOV		R2,#0x0 ;Initialise the sum to 0
 			MOV		R3,#0x0
-LOOP			ANDS		R7,R1,#1 ;R7 is -1 if first bit of R1 is 1
-			BEQ		SKIP
-			RSB		R6,R8,#32
-			ADDS		R2,R2,R0, LSL R8
-			ADC		R3,R3,R0, LSR R6 ;Add with a carry if carry set
-SKIP			ADD		R8,R8,#1
 			LSRS		R1,R1,#1
+			BCC		LOOP
+ADDER		RSB		R5,R4,#32
+			ADDS		R2,R2,R0,LSL R4
+			ADC		R3,R3,R0,LSR R5
+LOOP			LSRS		R1,R1,#1
+			ADD		R4,R4,#1
+			BCS		ADDER
 			BNE		LOOP
 			LDMED	R13!,{R0,R1,R4-R12,LR}
 			MOV		PC,LR
