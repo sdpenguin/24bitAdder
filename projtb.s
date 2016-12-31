427c48fc2d3ffb928b0c24515fa24f4d64793c36
Waleed El-Geresy <noEmail>
2016-12-31 01:28:00 +0000

    Made more efficient by including one extra line in the subbranch

--- projtb.s
+++ projtb.s
@@ -64,10 +64,10 @@ MUL24X24		STMED	R13!,{R4-R12,LR}
 			MOV		R3,#0x0
 LOOP			ANDS		R7,R1,R4,LSL R5 ;R5 holds the ANDed values
 			BEQ		SKPADD
+			RSB		R6,R5,#32
 			ADDS		R2,R2,R0, LSL R5 ;If the result is non-zero then there exists a bit at that position
 			ADC		R3,R3,R0, LSR R6
 SKPADD		ADD		R5,R5,#1
-			SUB		R6,R6,#1
 			CMP		R5,#24 ;Compare R4 to 2^24 (this means that this is the 24th bit shift and this signals the end
 			BNE		LOOP
 			LDMED	R13!,{R4-R12,LR}
