fe7e18e0f1fa5d65027ca094b1529aa41d798662
Waleed El-Geresy <noEmail>
2016-12-31 03:10:48 +0000

    Made code 13 lines. Worst case now takes 325 cycles whichis more than before, but the minimum number of cycles is also much much lower

--- PartASubmit.s
+++ PartASubmit.s
@@ -1,21 +1,18 @@
 		MOV		R0,#1
-		MOV		R1,#2
+		MOV		R1,#0xF0000000
 		BL		MUL24X24
 		END
 		
-MUL24X24	STMED	R13!,{R4-R12,LR}
-		MOV		R4,#0x1 ;This is to be used as the AND mask for R2
-		MOV		R5,#0x0 ;The amount of shift
-		MOV		R6,#32 ;The amount of shift for R3
+MUL24X24	STMED	R13!,{R0,R1,R4-R12,LR}
+		MOV		R8,#0x0 ;Second part of R0
 		MOV		R2,#0x0 ;Initialise the sum to 0
 		MOV		R3,#0x0
-LOOP		ANDS		R7,R1,R4,LSL R5 ;R5 holds the ANDed values
-		BEQ		SKPADD
-		RSB		R6,R5,#32
-		ADDS		R2,R2,R0, LSL R5 ;If the result is non-zero then there exists a bit at that position
-		ADC		R3,R3,R0, LSR R6
-SKPADD	ADD		R5,R5,#1
-		CMP		R5,#24 ;Compare R4 to 2^24 (this means that this is the 24th bit shift and this signals the end
+LOOP		MVNS		R7,R1, LSL #31 ;R7 is -1 if first bit of R1 is 1
+		ADDSGE	R2,R2,R0
+		ADCGE	R3,R3,R8 ;Add with a carry if carry set
+		LSLS		R0,R0,#1 ;The first part
+		ADC		R8,R8,R8 ;R8 is a left cont. of R2
+		LSRS		R1,R1,#1
 		BNE		LOOP
-		LDMED	R13!,{R4-R12,LR}
+		LDMED	R13!,{R0,R1,R4-R12,LR}
 		MOV		PC,LR
--- projtb.s
+++ projtb.s
@@ -56,21 +56,20 @@ PROJTBDATA15	DCD		0x103D62, 0x4380C7, 0x3BF6B72E, 0x448
 			;------------------------------------------------------------------------------------------------
 			
 			
-MUL24X24		STMED	R13!,{R4-R12,LR}
-			MOV		R4,#0x1 ;This is to be used as the AND mask for R2
-			MOV		R5,#0x0 ;The amount of shift
-			MOV		R6,#32 ;The amount of shift for R3
+MUL24X24		STMED	R13!,{R0,R1,R4-R12,LR}
+			MOV		R4,#0x0 ;A zero
+			MOV		R8,#0x0 ;Second part of R0
 			MOV		R2,#0x0 ;Initialise the sum to 0
 			MOV		R3,#0x0
-LOOP			ANDS		R7,R1,R4,LSL R5 ;R5 holds the ANDed values
-			BEQ		SKPADD
-			RSB		R6,R5,#32
-			ADDS		R2,R2,R0, LSL R5 ;If the result is non-zero then there exists a bit at that position
-			ADC		R3,R3,R0, LSR R6
-SKPADD		ADD		R5,R5,#1
-			CMP		R5,#24 ;Compare R4 to 2^24 (this means that this is the 24th bit shift and this signals the end
+LOOP			ANDS		R7,R1,#1 ;R7 holds the ANDed values
+			BEQ		SKPADD ;If zero then skip adding
+			ADDS		R2,R2,R0
+			ADC		R3,R3,R8
+SKPADD		LSLS		R0,R0,#1 ;The first part
+			ADC		R8,R4,R8, LSL #1 ;R8 is a left cont. of R2
+			LSRS		R1,R1,#1
 			BNE		LOOP
-			LDMED	R13!,{R4-R12,LR}
+			LDMED	R13!,{R0,R1,R4-R12,LR}
 			MOV		PC,LR
 			
 			;TODO:	Need to fix it for filling into R2 and R3
